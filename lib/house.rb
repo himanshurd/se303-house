55b9ae1cf75a266118895ca03fedd6532de8861b
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-04-14 12:34:15 -0700

    Pass the test_line_4 with shameless green approach Reference #4

--- lib/house.rb
+++ lib/house.rb
@@ -3,11 +3,13 @@ class House
     def line(number)
         case number
         when 1
-             "This is the house that Jack built.\n"
+            "This is the house that Jack built.\n"
         when 2
             "This is the malt that lay in the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
+        when 4
+            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 end
