0820a34bb8f4de0f4e1fd5e4385265ae77ca02d8
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-04-21 12:03:33 -0700

    Complete refactor recite Method

--- lib/house.rb
+++ lib/house.rb
@@ -31,18 +31,6 @@ class House
 
   def recite()
     (1..12).map {|number| line(number)}.join("\n")
-    # "#{line(1)}\n"+
-    # "#{line(2)}\n"+
-    # "#{line(3)}\n"+
-    # "#{line(4)}\n"+
-    # "#{line(5)}\n"+
-    # "#{line(6)}\n"+
-    # "#{line(7)}\n"+
-    # "#{line(8)}\n"+
-    # "#{line(9)}\n"+
-    # "#{line(10)}\n"+
-    # "#{line(11)}\n"+
-    # "#{line(12)}"
   end
   
 end
