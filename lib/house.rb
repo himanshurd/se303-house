769d36fb2345b014185e5cc07cf4094c056fe111
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-06-01 15:24:53 -0700

    Change the lyrics ordering in lyrics function Check the issue for reasoning Reference #54

--- lib/house.rb
+++ lib/house.rb
@@ -17,21 +17,20 @@ class House
     (1..12).map {|number| line(number)}.join("\n")
   end
 
-
   def lyrics
     [
-      "the house that Jack built.\n",
-      "the malt that lay in",
-      "the rat that ate",
-      "the cat that killed",
-      "the dog that worried",
-      "the cow with the crumpled horn that tossed",
-      "the maiden all forlorn that milked",
-      "the man all tattered and torn that kissed",
-      "the priest all shaven and shorn that married",
-      "the rooster that crowed in the morn that woke",
+      "the horse and the hound and the horn that belonged to",
       "the farmer sowing his corn that kept",
-      "the horse and the hound and the horn that belonged to"
+      "the rooster that crowed in the morn that woke",
+      "the priest all shaven and shorn that married",
+      "the man all tattered and torn that kissed",
+      "the maiden all forlorn that milked",
+      "the cow with the crumpled horn that tossed",
+      "the dog that worried",
+      "the cat that killed",
+      "the rat that ate",
+      "the malt that lay in",
+      "the house that Jack built.\n"
     ]
   end
 end
