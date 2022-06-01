f6e79fa7b6798ba9730795c0fc20b3a4a379a022
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-06-01 11:39:09 -0700

    replace line of code with recursive function call in verse 4 of episode function Reference #20

--- lib/house.rb
+++ lib/house.rb
@@ -7,9 +7,9 @@ class House
     when 2
       "the malt that lay in #{episodes(number-1)}"
     when 3
-      "the rat that ate #{episodes(number-1)} "
+      "the rat that ate #{episodes(number-1)}"
     when 4
-      "the cat that killed the rat that ate the malt that lay in "
+      "the cat that killed #{episodes(number-1)}"
     when 5
       "the dog that worried the cat that killed the rat that ate the malt that lay in "
     when 6
