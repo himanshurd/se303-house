1a1909772b23339df052155415c837b236d20d89
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-06-01 14:29:45 -0700

    Remove case statement when 6  Reference #48

--- lib/house.rb
+++ lib/house.rb
@@ -12,8 +12,6 @@ class House
       "#{lyrics[number-1]} #{episodes(number-1)}"
     when 5
       "#{lyrics[number-1]} #{episodes(number-1)}"
-    when 6
-      "#{lyrics[number-1]} #{episodes(number-1)}"
     else
       "#{lyrics[number-1]} #{episodes(number-1)}"  
     end
