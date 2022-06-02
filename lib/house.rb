ff81e98abfb53f760cc3fb95b4c2a79099344ebd
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-06-02 09:42:58 -0700

    Remove the lyrics method that have the lyrics in it Reference #60

--- lib/house.rb
+++ lib/house.rb
@@ -12,28 +12,28 @@ class House
     (1..12).map {|number| line(number)}.join("\n")
   end
 
-  def lyrics
-    [
-      "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate",
-      "the malt that lay in",
-      "the house that Jack built.\n"
-    ]
-  end
+  # def lyrics
+  #   [
+  #     "the horse and the hound and the horn that belonged to",
+  #     "the farmer sowing his corn that kept",
+  #     "the rooster that crowed in the morn that woke",
+  #     "the priest all shaven and shorn that married",
+  #     "the man all tattered and torn that kissed",
+  #     "the maiden all forlorn that milked",
+  #     "the cow with the crumpled horn that tossed",
+  #     "the dog that worried",
+  #     "the cat that killed",
+  #     "the rat that ate",
+  #     "the malt that lay in",
+  #     "the house that Jack built.\n"
+  #   ]
+  # end
 
   def noun
     [ "the horse and the hound and the horn",
       "the farmer sowing his corn",
       "the rooster that crowed in the morn",
-      "the priest all shaven and shorn ",
+      "the priest all shaven and shorn",
       "the man all tattered and torn",
       "the maiden all forlorn",
       "the cow with the crumpled horn",
@@ -41,6 +41,7 @@ class House
       "the cat",
       "the rat",
       "the malt",
+      "the house"
     ]
   end
 
@@ -56,7 +57,8 @@ class House
       "that worried",
       "that killed",
       "that ate",
-      "that lay in"
+      "that lay in",
+      "that Jack built.\n"
     ]
   end
 
