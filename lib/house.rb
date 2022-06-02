f777beccf371f02f2c40b1d941598b0dce3c1df1
himanshurd <91275023+himanshurd@users.noreply.github.com>
2022-06-02 10:29:49 -0700

    Copy noun and actions from House class to LyricsHouse class Reference #65

--- lib/house.rb
+++ lib/house.rb
@@ -57,6 +57,38 @@ class RandomHouse < House
   end
 end
 
-class LyricsHouse
+class LyricsHouse < House
   
+  def noun
+    [ "the horse and the hound and the horn",
+      "the farmer sowing his corn",
+      "the rooster that crowed in the morn",
+      "the priest all shaven and shorn",
+      "the man all tattered and torn",
+      "the maiden all forlorn",
+      "the cow with the crumpled horn",
+      "the dog",
+      "the cat",
+      "the rat",
+      "the malt",
+      "the house"
+    ]
+  end
+
+  def actions
+    [
+      "that belonged to",
+      "that kept",
+      "that woke",
+      "that married",
+      "that kissed",
+      "that milked",
+      "that tossed",
+      "that worried",
+      "that killed",
+      "that ate",
+      "that lay in",
+      "that Jack built.\n"
+    ]
+  end
 end
